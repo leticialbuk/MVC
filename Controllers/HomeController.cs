4fd5598091c71e8000d62ed7bf0766b85c6da9de
leticialbuk <leticia.albuk@gmail.com>
2023-08-02 08:07:49 -0300

    entendo padrão MVC e criando controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() 
+        {
+            return "Hello";        
+        }
+    }
+}
