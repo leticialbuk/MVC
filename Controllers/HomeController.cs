eb7794845929b0cca1722311a332d9fbb2305c70
leticialbuk <leticia.albuk@gmail.com>
2023-08-03 07:50:51 -0300

    Adicionando suporte aos  controllers, criando registros e lendo itens do banco através do Postaman

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
 
 namespace Todo.Controllers
 {
     [ApiController]
     public class HomeController : ControllerBase
     {
-        [HttpGet]
-        public string Get() 
+        [HttpGet("/")]
+        public List<TodoModel> Get([FromServices]AppDbContext context) 
         {
-            return "Hello";        
+            return context.Todos.ToList();        
+        }
+
+        [HttpPost("/")]
+        public TodoModel Post([FromBody]TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();  
+
+            return todo;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Todo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext> ();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
