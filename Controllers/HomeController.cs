ee959c8a27aa8aad62c092ecffa400fb11f43747
leticialbuk <leticia.albuk@gmail.com>
2023-08-04 07:37:14 -0300

    pequeno crud em mvc/api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,19 +8,58 @@ namespace Todo.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public List<TodoModel> Get([FromServices]AppDbContext context) 
+        public List<TodoModel> Get(
+            [FromServices]AppDbContext context) 
         {
             return context.Todos.ToList();        
         }
 
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById([FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            return context.Todos.FirstOrDefault(x=>x.Id == id);
+        }
+
         [HttpPost("/")]
-        public TodoModel Post([FromBody]TodoModel todo,
+        public TodoModel Post(
+            [FromBody]TodoModel todo,
             [FromServices] AppDbContext context)
         {
             context.Todos.Add(todo);
             context.SaveChanges();  
 
             return todo;
+        }
+
+        [HttpPut("/{id:int}")]
+        public TodoModel Put(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x=>x.Id == id);
+            if(model == null) 
+            {
+                return todo;            
+            }
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+            return model;
+        }
+
+        [HttpDelete("/{id:int}")]
+        public TodoModel Delete(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            context.Todos.Update(model);
+            context.SaveChanges();
+            return model;
         }
     }
 }
